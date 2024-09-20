c77a73309555ba6e6a2ae2bd7a6a898334f2ae90
Aidan Weeks <aidanweeks1066@gmail.com>
2024-09-20 15:40:49 +0100

    Initial commit

---
+++ src/main/kotlin/ie/setu/controlflow/main.kt
@@ -0,0 +1,148 @@
+package ie.setu.controlflow
+
+import java.lang.Integer.parseInt
+
+fun main(args: Array<String>) {
+    println("The max number is: " + maxOf(4,6));
+    printValue(7)
+    printRange(30)
+    println(printExpressionResult(12,"12"))
+    printInferredType(4.0)
+    println(printAnimal("elephant"))
+    println(describe(1))
+    println(describe("Hello"))
+    println(describe(1000L))
+    println(describe(2))
+    println(describe("other"))
+    forEachStandard()
+    forEachIndices()
+    forEachWithIndex()
+    whileWithList()
+    doWhileExample()
+    workingWithStrings()
+
+
+}
+
+fun maxOf(a: Int, b: Int) =
+    if (a > b) {
+        print("a is greater than b. ")
+        a
+    }else {
+        print("b is greater than a. ")
+        b
+    }
+
+fun printValue(value : Int){
+    when(value){
+        1 -> "The value is One"
+        2-> "the value is Two"
+        in 3..5 -> "The value is between Three and Five"
+        6, 7, 8 -> "The value is either six, seven or eight"
+        else -> {
+            "The value is: " + value
+        }
+    }
+}
+
+fun printRange(value : Int){
+    when(value){
+        in 1..10 -> println("between1..10");
+        !in 10..20 -> println("Outside 10..20")
+    }
+}
+
+fun printExpressionResult(numberValue: Int, stringValue: String){
+    when (numberValue){
+        parseInt(stringValue) -> "stringValue holds the same number as numberValue"
+        else -> {
+           "StringValue is not the same number as numberValue"
+        }
+    }
+}
+
+fun printInferredType(numberValue: Any){
+    when(numberValue){
+        is Int -> println("The inferred type is Int")
+        !is Int -> println("the inferred type is NOT Int")
+    }
+}
+
+fun printAnimal(aString: String) = when {
+    aString == "Dog" -> "a dog"
+    aString == "Cat" ->"A cat"
+    aString.contains("pig") -> "A pig of some sort"
+    aString.isEmpty() -> "Empty String... no animal"
+    else -> {
+        "no identifiable animal"
+    }
+}
+
+fun describe(obj: Any) = when (obj) {
+        1 -> "One"
+        "Hello" -> "A greeting"
+        is Long -> "Long Var"
+        !is String -> "Not a String"
+        else -> "unknown"
+    }
+
+fun forEachStandard(){
+    val items = listOf("apple", "banana", "kiwi")
+    for (item in items){
+        println("Item is: " + item)
+    }
+}
+
+fun forEachIndices(){
+    val items = listOf("apple", "banana", "kiwi")
+    for (index in items.indices){
+        println("Item at " + index + " is: " + items[index])
+    }
+}
+
+fun forEachWithIndex(){
+    val items = listOf("apple", "banana", "kiwi")
+    for ((index, value) in items.withIndex()){
+        println("The item at " + index + " is: " + value)
+    }
+}
+
+fun whileWithList(){
+    val items = listOf("apple", "banana", "kiwi")
+    var index = 0
+    while (index < items.size){
+        println ("Item at index: " + index + " is: " +  items[index])
+        index++
+    }
+}
+
+fun doWhileExample(){
+    var index = 0
+    do {
+        println("Do While Index is: " + index)
+        index++
+    } while (index < 5)
+}
+
+fun workingWithStrings(){
+    val aString = "An escaped string!\n"
+    print(aString)
+
+    val einstein = """
+        Learn from yesterday
+        Live for today
+        Hope for tomorrow
+        The important thing
+        is not to stop questioning.
+        (Albert Einstein)
+    """.trimIndent()
+    println(einstein)
+
+    val franklin = """
+          |Tell me and I forget.
+          |Teach me and I remember.
+          |Involve me and I learn.
+          |  (Benjamin Franklin)
+    """.trimMargin()
+    println(franklin)
+}
